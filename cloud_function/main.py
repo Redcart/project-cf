2ab6225825c9846817aa6d7ed3a4802bc5b3ab6d
Simon Corde <scorde@MacBook-Pro-de-Simon.local>
2024-11-11 23:19:04 +0100

    add type for ingestion date in pandas reading

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,15 +1,17 @@
+from datetime import datetime
+
+import pytz
+
 from utils import get_data, transform_data, ingest_data
+
+
 URL = "https://api.publibike.ch/v1/public/partner/stations"
 BUCKET_NAME = "ind-etl-publibike-dev"
 PROJECT_ID = "gold-circlet-433614-k2"
 
-from datetime import datetime
-
-
-
 def extract_transform_load(request):
 
-    now = datetime.now()
+    now = datetime.now(pytz.timezone('Europe/Paris'))
     current_full_date = now.strftime("%Y-%m-%d %H:%M:00")
     current_ymd = now.strftime("%Y-%m-%d")
     current_hour = now.strftime("%H:00:00")

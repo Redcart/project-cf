c20b3eee75f46b2ee1b48f5f21206f2407415088
Simon Corde <scorde@MacBook-Pro-de-Simon.local>
2024-11-11 19:41:01 +0100

    add type for ingestion date in pandas reading

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -13,6 +13,7 @@ def extract_transform_load(request):
     current_full_date = now.strftime("%Y-%m-%d %H:%M:00")
     current_ymd = now.strftime("%Y-%m-%d")
     current_hour = now.strftime("%H:00:00")
+    current_minute = now.strftime("%H:%M:00")
 
     get_data(
         url=URL, 
@@ -23,7 +24,7 @@ def extract_transform_load(request):
     transform_data(
         input_path=f"raw_data/{current_ymd}/{current_hour}/data.json",
         bucket_name=BUCKET_NAME,
-        output_path=f"transformed_data/{current_ymd}/{current_hour}/transformed_data_stations.csv",
+        output_path=f"transformed_data/{current_ymd}/{current_hour}/{current_minute}_transformed_data_stations.csv",
         mode="stations",
         date_time=current_full_date
     )
@@ -31,13 +32,13 @@ def extract_transform_load(request):
     transform_data(
         input_path=f"raw_data/{current_ymd}/{current_hour}/data.json",
         bucket_name=BUCKET_NAME,
-        output_path=f"transformed_data/{current_ymd}/{current_hour}/transformed_data_bikes.csv",
+        output_path=f"transformed_data/{current_ymd}/{current_hour}/{current_minute}_transformed_data_bikes.csv",
         mode="capacity",
         date_time=current_full_date
     )
 
     ingest_data(
-        input_path=f"transformed_data/{current_ymd}/{current_hour}/transformed_data_stations.csv", 
+        input_path=f"transformed_data/{current_ymd}/{current_hour}/{current_minute}_transformed_data_stations.csv", 
         bucket_name=BUCKET_NAME,
         project_id=PROJECT_ID,
         dataset="publibike_dev", 
@@ -46,7 +47,7 @@ def extract_transform_load(request):
     )
 
     ingest_data(
-        input_path=f"transformed_data/{current_ymd}/{current_hour}/transformed_data_bikes.csv", 
+        input_path=f"transformed_data/{current_ymd}/{current_hour}/{current_minute}_transformed_data_bikes.csv", 
         bucket_name=BUCKET_NAME,
         project_id=PROJECT_ID,
         dataset="publibike_dev", 

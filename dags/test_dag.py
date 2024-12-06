1e6880c833b03d07ab1d3efd0ba0a2993a9eb031
Simon Corde <scorde@MacBook-Pro-de-Simon.local>
2024-12-06 20:24:03 +0100

    no catchup for airflow dag test

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -5,7 +5,7 @@ from datetime import datetime
 def my_callable(*args, **kwargs):
     print("Hello from PythonOperator")
 
-with DAG('my_dag', schedule_interval='*/10 * * * *', start_date=datetime(2021, 1, 1)) as dag:
+with DAG('my_dag', schedule_interval='*/10 * * * *', start_date=datetime(2021, 1, 1), catchup=False) as dag:
     python_task = PythonOperator(
         task_id='my_python_task',
         python_callable=my_callable

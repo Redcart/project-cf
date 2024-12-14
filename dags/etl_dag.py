e20cdbbb3a89a8c4d2dd480e8ebfb45239de1d0c
Simon Corde <scorde@MacBook-Pro-de-Simon.local>
2024-12-14 20:21:44 +0100

    add dag for etl

---
+++ dags/etl_dag.py
@@ -0,0 +1,32 @@
+from datetime import timedelta
+
+import airflow
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+from utils import extract_transform_load
+
+
+URL = "https://api.publibike.ch/v1/public/partner/stations"
+BUCKET_NAME = "ind-etl-publibike-dev"
+PROJECT_ID = "inspired-victor-442419-j3"
+
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(0),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG('my_dag', schedule_interval='*/5 * * * *', default_args=default_args, catchup=False) as dag:
+
+    python_task = PythonOperator(
+        task_id='etl_task',
+        python_callable=extract_transform_load,
+        op_kwargs={
+            "url": URL, 
+            "project_id": PROJECT_ID, 
+            "bucket_name": BUCKET_NAME, 
+            "mode": "capacity"
+        },
+    )
+    

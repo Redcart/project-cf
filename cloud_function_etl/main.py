892ae537b6154eadd882da9f029949221d9c878a
Simon Corde <scorde@MacBook-Pro-de-Simon.local>
2024-12-15 20:16:13 +0100

    add dag for etl

--- cloud_function_etl/main.py
+++ cloud_function_etl/main.py
@@ -1,18 +1,19 @@
 from datetime import datetime
 import json
+import logging 
 
 import pytz
 
 from utils import get_data, transform_data, ingest_data
 
 URL = "https://api.publibike.ch/v1/public/partner/stations"
-BUCKET_NAME = "ind-etl-publibike-dev"
-PROJECT_ID = "gold-circlet-433614-k2"
+BUCKET_NAME = "proj-etl-publibike-dev"
+PROJECT_ID = "inspired-victor-442419-j3"
 
 def extract_transform_load(request):
 
     mode = json.loads(request.data.decode()).get("mode")
-    print(mode)
+    logging.info(f" mode: {mode}")
 
     now = datetime.now(pytz.timezone('UTC'))
     current_full_date = now.strftime("%Y-%m-%d %H:%M:00")
@@ -40,7 +41,7 @@ def extract_transform_load(request):
             input_path=f"transformed_data/{current_ymd}/{current_hour}/{current_minute}_transformed_data_stations.csv", 
             bucket_name=BUCKET_NAME,
             project_id=PROJECT_ID,
-            dataset="publibike_dev", 
+            dataset="publibike", 
             table="stations", 
             mode="stations"
         )
@@ -59,7 +60,7 @@ def extract_transform_load(request):
             input_path=f"transformed_data/{current_ymd}/{current_hour}/{current_minute}_transformed_data_bikes.csv", 
             bucket_name=BUCKET_NAME,
             project_id=PROJECT_ID,
-            dataset="publibike_dev", 
+            dataset="publibike", 
             table="capacity", 
             mode="capacity"
         )

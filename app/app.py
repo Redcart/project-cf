50be60642244859381e94315a3183288007969ce
Simon Corde <scorde@MacBook-Pro-de-Simon.local>
2025-01-09 21:41:50 +0100

    fix dataset name

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 from google.cloud import bigquery
 import streamlit as st
 
-PROJECT_ID = "gold-circlet-433614-k2"
+PROJECT_ID = "inspired-victor-442419-j3"
 bigquery_client = bigquery.Client(project=PROJECT_ID)
 
 sql_query = f"""
     SELECT
         * 
-    FROM `gold-circlet-433614-k2.publibike_dev.stations_capacity_aggregated`
+    FROM `inspired-victor-442419-j3.publibike.stations_capacity_aggregated`
     ORDER BY station_id, ingestion_time
 """
 
@@ -15,6 +15,6 @@ df_stations_capacity_aggregated = bigquery_client.query(sql_query).to_dataframe(
 
 df_filtered = df_stations_capacity_aggregated.loc[df_stations_capacity_aggregated["station_id"] == "10", ]
 
-st.dataframe(df_filtered )
+st.dataframe(data=df_filtered)
 
 st.line_chart(data=df_filtered, x="ingestion_time", y="nb_bikes_available")
